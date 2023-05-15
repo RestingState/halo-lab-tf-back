80ccb3b80181e3a6c7f0c982a4fd0655586543f7
RestingState <zaharkevich.denis@gmail.com>
2023-05-15 08:42:19 +0300

    changed User table

---
+++ prisma/migrations/20230515050816_added_password_column/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `username` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(30)`.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "password" VARCHAR(30) NOT NULL,
+ALTER COLUMN "username" SET DATA TYPE VARCHAR(30);
