64837db5dc753000386abddf72606130e6c161c9
RestingState <zaharkevich.denis@gmail.com>
2023-05-22 11:17:13 +0300

    removed console logs

--- src/utils.ts
+++ src/utils.ts
@@ -6,9 +6,7 @@ export function shuffle<T>(array: T[]) {
   const arrClone = structuredClone(array);
   for (let i = arrClone.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    console.log({ i, j, arrClone });
     [arrClone[i], arrClone[j]] = [arrClone[j], arrClone[i]];
-    console.log({ i, j, arrClone });
   }
   return arrClone;
 }
