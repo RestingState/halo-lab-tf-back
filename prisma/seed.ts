6093b1600d65c84037e2401cf8d140487aab36d0
RestingState <zaharkevich.denis@gmail.com>
2023-05-22 16:41:45 +0300

    added seeds and readme

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient, BoardCellValue } from '@prisma/client';
+import boardService from '../src/services/board.service';
+const prisma = new PrismaClient();
+
+const boards: {
+  matrix: BoardCellValue[][];
+  exitCell: { x: number; y: number };
+  positionsSuitableForUserStart: { x: number; y: number }[];
+}[] = [
+  {
+    matrix: [
+      ['p', 'p', 'w', 'p', 'p', 'p', 'p', 'p'],
+      ['w', 'p', 'w', 'p', 'w', 'w', 'w', 'p'],
+      ['p', 'p', 'w', 'p', 'p', 'w', 'p', 'p'],
+      ['p', 'w', 'w', 'w', 'p', 'w', 'w', 'w'],
+      ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+      ['w', 'w', 'w', 'w', 'w', 'p', 'w', 'p'],
+      ['w', 'p', 'p', 'p', 'p', 'p', 'w', 'w'],
+      ['p', 'p', 'w', 'w', 'w', 'p', 'p', 'p'],
+    ],
+    exitCell: { x: 7, y: 7 },
+    positionsSuitableForUserStart: [
+      { x: 0, y: 0 },
+      { x: 2, y: 6 },
+    ],
+  },
+];
+
+async function main() {
+  await prisma.board.deleteMany();
+  await boardService.fillBoardCells();
+
+  for (const board of boards) {
+    await boardService.createBoard(board);
+  }
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async e => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
